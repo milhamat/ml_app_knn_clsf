593228e5008015c115eabe2aaaa308f114d2b1ea
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2022-02-15 15:53:21 +0800

    first running with warning in the X

--- machine.py
+++ machine.py
@@ -1,39 +1,39 @@
-import os
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-path = 'D:/STUDY/Self Study/3.MachineLearning_and_DataScience/Python/x.Project/ml_app_knn_clsf'
+path = 'D:/STUDY/Self Study/3.MachineLearning_and_DataScience/Python/x.Project/ml_app_knn_clsf/'
 data = pd.read_csv(path + "Smarket.csv")
 
 
-def chg_dtyp(df):
-    """
-    df : is the feature or column from the y target
-    this helper help change the data type
-    from object(string) into integer
-    0 for Down
-    1 for Up
-    """
-    list = []
-    for data in df:
-        if data == 'Up':
-            list.append(1)
-        else:
-            list.append(0)
-        return list
+# def chg_dtyp(df):
+#     """
+#     df : is the feature or column from the y target
+#     this helper help change the data type
+#     from object(string) into integer
+#     0 for Down
+#     1 for Up
+#     """
+#     list = []
+#     for data in df:
+#         if data == 'Up':
+#             list.append(1)
+#         else:
+#             list.append(0)
+#         return list
 
 
-data_list = chg_dtyp(data.Direction)
-data['Outcome'] = data_list
-new_data = data.drop(columns=["Year", "Direction", "Unnamed: 0"], axis=1)
+# data_list = chg_dtyp(data.Direction)
+# data['Outcome'] = data_list
+# new_data = data.drop(columns=["Year", "Direction", "Unnamed: 0"], axis=1)
+new_data = data.drop(columns=["Year", "Unnamed: 0"], axis=1)
 
 scale = StandardScaler()
-X = pd.DataFrame(scale.fit_transform(new_data.drop("Outcome", axis=1),),
+X = pd.DataFrame(scale.fit_transform(new_data.drop("Direction", axis=1),),
                  columns=['Lag1', 'Lag2', 'Lag3', 'Lag4', 'Lag5', 'Volume', 'Today'])
-y = new_data.Outcome
+y = new_data.Direction
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.3,
@@ -52,11 +52,8 @@ Volume = 1.59
 Today = 2.75
 
 new_val = np.array([[Lag1, Lag2, Lag3, Lag4, Lag5, Volume, Today]])
-print(f'new_val shape : {new_val.shape}')
+#print(f'new_val shape : {new_val.shape}')
 
 prediction = knn.predict(new_val)
 
-if prediction == 1:
-    print('Up')
-else:
-    print('Down')
+print(prediction)
