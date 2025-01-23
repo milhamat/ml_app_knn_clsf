74806b354cc738dd95e2b627e281f5fc8a24c58a
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-23 13:10:55 +0700

    fix the machine.py

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from src.models.machine import MarketKNN
+
+model = MarketKNN()
 
 screen = Tk()
 screen.title("SMarket Prediction")
@@ -13,9 +16,7 @@ name_feature = ['lag1', 'lag2', 'lag3', 'lag4', 'lag5', 'vol', 'tdy']
 entries = []
 
 for n in range(7):
-    print(n)
     Label(screen, text=name_feature[n]).grid(row=n, pady=5)
-    
     entry = Entry(screen)
     entry.grid(row=n, column=1, pady=5)
     entries.append(entry) 
--- src/models/machine.py
+++ src/models/machine.py
@@ -1,31 +1,18 @@
 import pickle
 import numpy as np
-import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
 
 class MarketKNN:
 
-
-    def __init__(self, lag1, lag2, lag3, lag4, lag5, vol, tdy, model_path='artifacts/model.pkl'):
-        self.lag1 = 0
-        self.lag2 = 0
-        self.lag3 = 0
-        self.lag4 = 0
-        self. lag5 = 0
-        self.vol = 0
-        self.tdy = 0
+    def __init__(self, model_path='artifacts/model.pkl'):
         #self.path = 'D:/STUDY/Self Study/3.MachineLearning_and_DataScience/Python/x.Project/ml_app_knn_clsf/'
         #self.data = pd.read_csv(self.path + "Smarket.csv")
         self.model_path = model_path
 
-    def predict(self):
+    def predict(self,lag1, lag2, lag3, lag4, lag5, vol, tdy):
 
         with open('model.pkl', 'rb') as f:
             model = pickle.load(f)
 
-        new_val = np.array([[self.lag1, self.lag2, self.lag3, self.lag4, self.lag5, self.vol, self.tdy]])
+        new_val = np.array([[lag1, lag2, lag3, lag4, lag5, vol, tdy]])
         prediction = model.predict(new_val)
         return ''.join(prediction)
