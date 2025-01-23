208c4ea9530fb2eab1f4590b1dcf79533625edd7
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-23 12:31:35 +0700

    initial ui on gui folder

---
+++ src/gui/ui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+screen = tk.Tk()
+screen.title("SMarket Prediction")
+
+# Adjust size
+screen.geometry("500x500")
+# set minimum window size value
+screen.minsize(300, 300)
+# set maximum window size value
+screen.maxsize(500, 500)
+
+screen.mainloop()
