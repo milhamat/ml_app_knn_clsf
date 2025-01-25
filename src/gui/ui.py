80c60801834611d30385b6cb6fed10bd879543c5
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-25 20:35:53 +0700

    make class for the interface

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -4,61 +4,65 @@ from src.models.machine import MarketKNN
 
 model = MarketKNN()
 
-screen = Tk()
-screen.title("SMarket Prediction")
-# Adjust size
-screen.geometry("400x400")
-# set minimum window size value
-screen.minsize(150, 150)
-# set maximum window size value
-screen.maxsize(400, 400)
-
-name_feature = ['lag1', 'lag2', 'lag3', 'lag4', 'lag5', 'vol', 'tdy']
-entries = []
-
-next = 0
-k=0
-for n in range(7):
-    Label(screen, text=name_feature[n]).grid(row=n, pady=5)
-    entry = Entry(screen)        
-    entry.grid(row=n, column=1, sticky = W, pady=5)
-    entries.append(entry)
-    next+=1 
-    k+=1
+class Interface:
+    def __init__(self):
+        pass
     
-    
-def gen_random():
-    for i, entry in enumerate(entries):
-        if name_feature[i] in ['lag1', 'lag2', 'lag3', 'lag4', 'lag5', 'tdy']:
-            random_value = round(random.uniform(-5.0, 6.0), 2)
-        elif name_feature[i] == 'vol':
-            random_value = round(random.uniform(0.4, 3.2), 2)
-        entry.delete(0, END)  # Clear the existing value in the Entry widget
-        entry.insert(0, str(random_value))  # Insert the random value as a string
-    
-    
+    def lunch(self):
+        screen = Tk()
+        screen.title("SMarket")
+        # Adjust size
+        screen.geometry("400x400")
+        # set minimum window size value
+        screen.minsize(150, 150)
+        # set maximum window size value
+        screen.maxsize(250, 300)
 
-# Function to update the predict label
-def update_prediction():
-    data = {name_feature[i]: entries[i].get() for i in range(len(name_feature))}
-    pred = model.predict(float(data['lag1']), 
-                         float(data['lag2']), 
-                         float(data['lag3']), 
-                         float(data['lag4']), 
-                         float(data['lag5']), 
-                         float(data['vol']), 
-                         float(data['tdy']))
-    predict_result.config(text=f"Predict Result : {pred}")  # Update the label's text
-    
+        name_feature = ['lag1', 'lag2', 'lag3', 'lag4', 'lag5', 'vol', 'tdy']
+        entries = []
 
-button = Button(screen, text="Predict", command=update_prediction)
-button.grid(row=len(name_feature), column=0, columnspan=1, pady=5)
+        next = 0
+        k=0
+        for n in range(7):
+            Label(screen, text=name_feature[n]).grid(row=n, pady=5)
+            entry = Entry(screen)        
+            entry.grid(row=n, column=1, sticky = W, pady=5)
+            entries.append(entry)
+            next+=1 
+            k+=1
+            
+            
+        def gen_random():
+            for i, entry in enumerate(entries):
+                if name_feature[i] in ['lag1', 'lag2', 'lag3', 'lag4', 'lag5', 'tdy']:
+                    random_value = round(random.uniform(-5.0, 6.0), 2)
+                elif name_feature[i] == 'vol':
+                    random_value = round(random.uniform(0.4, 3.2), 2)
+                entry.delete(0, END)  # Clear the existing value in the Entry widget
+                entry.insert(0, str(random_value))  # Insert the random value as a string
+            
+            
+        # Function to update the predict label
+        def update_prediction():
+            data = {name_feature[i]: entries[i].get() for i in range(len(name_feature))}
+            pred = model.predict(float(data['lag1']), 
+                                float(data['lag2']), 
+                                float(data['lag3']), 
+                                float(data['lag4']), 
+                                float(data['lag5']), 
+                                float(data['vol']), 
+                                float(data['tdy']))
+            predict_result.config(text=f"Predict Result : {pred}")  # Update the label's text
+            
 
-rd_button = Button(screen, text="Random", command=gen_random)
-rd_button.grid(row=len(name_feature), column=1, columnspan=1, pady=5)
+        button = Button(screen, text="Predict", command=update_prediction)
+        button.grid(row=len(name_feature), column=0, sticky = W, padx=5, pady=5)
 
-predict_result = Label(screen, text="Predict Result :")
-predict_result.grid(row=len(name_feature)+2)
+        rd_button = Button(screen, text="Random", command=gen_random)
+        rd_button.grid(row=len(name_feature), column=1, sticky = W, padx=5, pady=5)
+
+        predict_result = Label(screen, text="Predict Result :")
+        predict_result.grid(row=len(name_feature)+2, column=1, sticky = W, pady=5)
 
 
-screen.mainloop()
+        screen.mainloop()
