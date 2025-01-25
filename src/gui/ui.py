ceb8eb44627917212ecb8cdd00578f5ad8c34243
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-25 20:22:12 +0700

    add get random

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -1,3 +1,4 @@
+import random
 from tkinter import *
 from src.models.machine import MarketKNN
 
@@ -15,11 +16,26 @@ screen.maxsize(400, 400)
 name_feature = ['lag1', 'lag2', 'lag3', 'lag4', 'lag5', 'vol', 'tdy']
 entries = []
 
+next = 0
+k=0
 for n in range(7):
     Label(screen, text=name_feature[n]).grid(row=n, pady=5)
-    entry = Entry(screen)
-    entry.grid(row=n, column=1, pady=5)
-    entries.append(entry) 
+    entry = Entry(screen)        
+    entry.grid(row=n, column=1, sticky = W, pady=5)
+    entries.append(entry)
+    next+=1 
+    k+=1
+    
+    
+def gen_random():
+    for i, entry in enumerate(entries):
+        if name_feature[i] in ['lag1', 'lag2', 'lag3', 'lag4', 'lag5', 'tdy']:
+            random_value = round(random.uniform(-5.0, 6.0), 2)
+        elif name_feature[i] == 'vol':
+            random_value = round(random.uniform(0.4, 3.2), 2)
+        entry.delete(0, END)  # Clear the existing value in the Entry widget
+        entry.insert(0, str(random_value))  # Insert the random value as a string
+    
     
 
 # Function to update the predict label
@@ -32,15 +48,17 @@ def update_prediction():
                          float(data['lag5']), 
                          float(data['vol']), 
                          float(data['tdy']))
-    
     predict_result.config(text=f"Predict Result : {pred}")  # Update the label's text
     
 
 button = Button(screen, text="Predict", command=update_prediction)
-button.grid(row=len(name_feature), column=0, columnspan=2, pady=10)
+button.grid(row=len(name_feature), column=0, columnspan=1, pady=5)
+
+rd_button = Button(screen, text="Random", command=gen_random)
+rd_button.grid(row=len(name_feature), column=1, columnspan=1, pady=5)
 
 predict_result = Label(screen, text="Predict Result :")
-predict_result.grid(row=len(name_feature)+1)
+predict_result.grid(row=len(name_feature)+2)
 
 
 screen.mainloop()
