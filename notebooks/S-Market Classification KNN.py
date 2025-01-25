a79ec0cb8e85e3fc50f9205a5bdc7a4b5a985153
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-01-25 21:18:47 +0700

    add Requirements.txt

--- notebooks/S-Market Classification KNN.py
+++ notebooks/S-Market Classification KNN.py
@@ -8,8 +8,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-sns.set()
-get_ipython().run_line_magic('matplotlib', 'inline')
+# sns.set()
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 # In[2]:
